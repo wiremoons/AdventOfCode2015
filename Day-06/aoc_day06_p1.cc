232e1493eef602c98adc617a84bf353ba989dc40
Simon Rowe <simon@wiremoons.com>
2022-04-16 06:27:04 +0100

    Additional headers added Function added to count total lights on Function added to toggle light setting

--- Day-06/aoc_day06_p1.cc
+++ Day-06/aoc_day06_p1.cc
@@ -19,31 +19,73 @@
 //
 
 #include <algorithm>
+#include <cstddef>
 #include <fstream>
 #include <iostream>
+#include <numeric>
+#include <ostream>
 #include <string>
+#include <vector>
 
+///////////////////////////////////////////////////////////////////////////////
 // Notes on input format:
 //
 // Input data state changes are: 'toggle' 'turn on' 'turn off'
 // Range follows and is stated as:  887,9 through 959,629
 // Full example line of input for context:
 //    turn on 887,9 through 959,629
+///////////////////////////////////////////////////////////////////////////////
+
+/// @brief Count the total number of light in the 'on' state
+/// @param light_status is a boolean grid (matrix) of lights to be checked
+/// @ return the total number of lights that are on (ie set to true)
+int total_lights_on(std::vector<std::vector<bool>> const &light_status)
+{
+    int result{0};
+    for (const auto &row : light_status) {
+        for (const auto &col : row) {
+            if (col) {
+                result++;
+            }
+        }
+    }
+    return result;
+}
+
+/// @brief Alter the current light setting to it opposite setting
+/// @param light_status reference to a boolean grid (matrix) of lights to be toggled
+/// @ return
+void toggle_lights(std::vector<std::vector<bool>> &light_status)
+{
+
+    for (size_t i{0}; i < light_status.size(); i++) {
+        for (size_t j{0}; j < light_status[i].size(); j++) {
+            // light_status.at(i).at(j).flip(); // bounds check version
+            light_status[i][j].flip();
+        }
+    }
+}
 
 int main()
 {
 
-    std::string line;
-    std::ifstream input_file;
+    std::string line{};
+    std::ifstream input_file{};
+    // 0-999 grid if bool to track light on/off status - initialised to off
+    std::vector<std::vector<bool>> light_status{1000, std::vector<bool>(1000, false)};
     int count_changes{};
     int lights_on{};
+
+    lights_on = total_lights_on(light_status);
+    std::cout << "Initial lights on are: " << lights_on << '\n';
+    toggle_lights(light_status);
 
     // Switch comments for 'test'  vs  'final' data file
     // input_file.open("./data/day06-TEST-input.txt", std::ios::in);
     input_file.open("./data/day06-input.txt", std::ios::in);
 
-    if (!input_file.is_open()) {
-        std::cout << "Error opening puzzle date input file. Aborted." << std::endl;
+    if (not input_file.is_open()) {
+        std::cerr << "Error opening puzzle date input file. Aborted." << std::endl;
         std::exit(1);
     }
 
@@ -51,6 +93,9 @@ int main()
     // vector
     while (std::getline(input_file, line)) {
     }
+
+    // final check of total on
+    lights_on = total_lights_on(light_status);
 
     std::cout << std::endl << "Advent Of Code 2015 :  Day 06 Part 01" << '\n' << '\n';
     std::cout << "  » Number of switches analysed : '" << count_changes << "'" << '\n';
